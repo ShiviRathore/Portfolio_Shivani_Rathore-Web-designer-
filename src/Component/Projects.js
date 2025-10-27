c69f526059d8b1c213919ca3907fc4831b3b2495
shivani <shivanirathore093@gmail.com>
2025-10-27 16:33:59 +0530

    Updated components

--- src/Component/Projects.js
+++ src/Component/Projects.js
@@ -9,7 +9,7 @@ function Projects() {
           <h4 className='text-white mb-12 md:text-4xl text-3xl font-bold' data-aos="flip-right">My <span className='text-sky-500'>Projects</span></h4>
           <div className='grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 lg:gap-8 gap-4 md:w-full'>
            {/* <div className='relative group overflow-hidden border-4 border-[#0488CC] rounded-xl p-2 hover:scale-105 transition-transform duration-300'>
-              <img src="images/project1.png" alt="" className='lg:w-[397px] w-full h-[225px] rounded-xl' />
+              <img src="images/project1.png" alt="" className='lg:w-[397px] w-full h-[250px] rounded-xl' />
                <div className="absolute inset-0 bg-black/70 opacity-0 group-hover:opacity-100 transition duration-500 flex flex-col justify-center items-center text-center text-white p-4">
                   <h3 className="text-xl font-bold mb-2">MYTket Project</h3>
                   <p className="text-sm mb-4">React js, Tailwind CSS</p>
@@ -17,7 +17,7 @@ function Projects() {
                 </div>
             </div> */}
             <div className='relative group overflow-hidden border-4 border-[#0488CC] rounded-xl p-2 hover:scale-105 transition-transform duration-300'>
-              <img src="images/dashboard.png" alt="" className='lg:w-[397px] w-full h-[225px] rounded-xl' />
+              <img src="images/dashboard.png" alt="" className='lg:w-[397px] w-full h-[250px] rounded-xl' />
                <div className="absolute inset-0 bg-black/70 opacity-0 group-hover:opacity-100 transition duration-500 flex flex-col justify-center items-center text-center text-white p-4">
                   <h3 className="text-xl font-bold mb-2">AIM Project</h3>
                   <p className="text-sm mb-4">React js, Bootstrap</p> 
@@ -25,7 +25,7 @@ function Projects() {
                 </div>
             </div>            
             <div className='relative group overflow-hidden border-4 border-[#0488CC] rounded-xl p-2 hover:scale-105 transition-transform duration-300'>
-             <img src="images/qbit-ai.png" alt="" className='lg:w-[397px] w-full h-[225px] rounded-xl' />
+             <img src="images/qbit-ai.png" alt="" className='lg:w-[397px] w-full h-[250px] rounded-xl' />
               <div className="absolute inset-0 bg-black/70 opacity-0 group-hover:opacity-100 transition duration-500 flex flex-col justify-center items-center text-center text-white p-4">
                   <h3 className="text-xl font-bold mb-2">QBIT Project</h3>
                   <p className="text-sm mb-4">React js, Tailwind CSS</p> 
@@ -33,7 +33,7 @@ function Projects() {
                 </div>
             </div> 
             <div class="relative group overflow-hidden border-4 border-[#0488CC] rounded-xl p-2 hover:scale-105 transition-transform duration-300">
-                <img src="images/frs.png" alt="" className='lg:w-[397px] w-full h-[225px] rounded-xl' />
+                <img src="images/frs.png" alt="" className='lg:w-[397px] w-full h-[250px] rounded-xl' />
                 <div className="absolute inset-0 bg-black/70 opacity-0 group-hover:opacity-100 transition duration-500 flex flex-col justify-center items-center text-center text-white p-4">
                   <h3 className="text-xl font-bold mb-2">FRS Project</h3>
                   <p className="text-sm mb-4">HTML, Tailwind CSS</p>
